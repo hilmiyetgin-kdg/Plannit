3fa9d0cb5cccc82ab77e8642311eeaac576c555f
Hilmi Yetgin <hilmi.yetgin@student.kdg.be>
2024-07-22 12:48:14 +0200

    Initial commit

---
+++ src/main/java/com/hilmiyetgin/plannit/PlannitApplication.java
@@ -0,0 +1,13 @@
+package com.hilmiyetgin.plannit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hilmiyetgin/plannit/PlannitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilmiyetgin.plannit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
